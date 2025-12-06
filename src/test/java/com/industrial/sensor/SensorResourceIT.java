0cb7b7b0bab016c5ede21823c649c0c37ca79afd
Benjamim-EP <bannk18@gmail.com>
2025-12-06 14:12:37 -0300

    first commit

---
+++ src/test/java/com/industrial/sensor/SensorResourceIT.java
@@ -0,0 +1,8 @@
+package com.industrial.sensor;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class SensorResourceIT extends SensorResourceTest {
+    // Execute the same tests but in packaged mode.
+}
